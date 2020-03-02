8d8e7b1ac45e6c9c66575bf519530483debacb1c
Lee Raulin <leeraulin@gmail.com>
2020-03-02 09:40:45 -0500

    Replace Axios with HTML5 fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Header from "./Header";
 import ProductsContainer from "./ProductsContainer";
-import axios from "axios";
 
 const App = () => {
   const [department, setDepartment] = useState(null);
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://0ogofj3z44.execute-api.us-east-1.amazonaws.com/dev/products",
-      )
-      .then(result => {
-        console.log("Fetching products....");
-        console.log(result.data);
-        setProducts(result.data);
+    fetch("https://0ogofj3z44.execute-api.us-east-1.amazonaws.com/dev/products")
+      .then(result => result.json())
+      .then(data => {
+        setProducts(data);
       })
       .catch(e => console.log(e));
   }, []);
