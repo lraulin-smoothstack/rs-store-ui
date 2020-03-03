eb4a3b121d8ba4215daafdd9439867ee502d8aac
Lee Raulin <leeraulin@gmail.com>
2020-03-03 16:50:51 -0500

    Add place order api

--- src/api/shop.js
+++ src/api/shop.js
@@ -5,6 +5,37 @@
 const TIMEOUT = 100;
 const API_URL = "https://0ogofj3z44.execute-api.us-east-1.amazonaws.com/dev";
 
+const createOrder = ({
+  product_id = 0,
+  user_id = 0,
+  coupon_code = "",
+  quantity = 0,
+}) => ({
+  product_id,
+  user_id,
+  coupon_code,
+  quantity,
+  in_store: 1,
+  date: new Date().toISOString(),
+  ship_date: null,
+  status: "placed",
+  deleted: 0,
+});
+
+const placeOrder = async ({ product_id, user_id, coupon_code, quantity }) => {
+  const order = createOrder({ product_id, user_id, coupon_code, quantity });
+  try {
+    const result = await fetch(API_URL + "/orders", {
+      method: "post",
+      body: JSON.stringify(order),
+    });
+    const responseBody = await result.json();
+    console.log(responseBody);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 export default {
   getProducts: async () => {
     try {
@@ -15,6 +46,17 @@ export default {
       console.log(e);
     }
   },
-  buyProducts: (payload, cb, timeout) =>
-    setTimeout(() => cb(), timeout || TIMEOUT),
+  buyProducts: payload => {
+    console.log("Placing order...");
+    for (let item of payload) {
+      const user_id = 0; // TODO
+      const coupon_code = null; // TODO
+      placeOrder({
+        product_id: item.id,
+        quantity: item.quantity,
+        user_id,
+        coupon_code,
+      });
+    }
+  },
 };
