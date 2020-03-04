a0894409b89d611bacc1fc4407e6f3413013f332
Lee Raulin <leeraulin@gmail.com>
2020-03-04 15:43:45 -0500

    add sign in form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import FilterContainer from "../containers/FilterContainer";
+import UserContainer from "../containers/UserContainer";
 
 const Header = props => (
   <>
@@ -8,6 +9,7 @@ const Header = props => (
       <Navbar.Brand href="#home">Wu, Raulin & Co. Apparel</Navbar.Brand>
       <Nav className="mr-auto" />
       <FilterContainer />
+      <UserContainer />
     </Navbar>
   </>
 );
---
+++ src/containers/UserContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Button,
+  Col,
+  Form,
+  OverlayTrigger,
+  Popover,
+  Row,
+} from "react-bootstrap";
+
+const UserContainer = () => (
+  <OverlayTrigger
+    trigger="click"
+    placement="bottom"
+    overlay={
+      <Popover id="signinPopover">
+        <Popover.Content>
+          <Form>
+            <Form.Group as={Row} controlId="formPlaintextEmail">
+              {/* <Form.Label column sm="2"> */}
+              <Col sm="10">
+                <Form.Label>Email</Form.Label>
+                <Form.Control plaintext placeholder="email@example.com" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formPlaintextPassword">
+              <Col sm="10">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Col>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Popover.Content>
+      </Popover>
+    }
+  >
+    <Button variant="secondary">Sign In</Button>
+  </OverlayTrigger>
+);
+
+export default UserContainer;
